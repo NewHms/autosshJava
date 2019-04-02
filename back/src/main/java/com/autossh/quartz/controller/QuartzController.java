574105ab404e0acb3505002df1fedebe0b30c2fc
NewHms <37338418+NewHms@users.noreply.github.com>
2019-04-02 11:38:12 +0800

    添加了动态更新任务时间

--- back/src/main/java/com/autossh/quartz/controller/QuartzController.java
+++ back/src/main/java/com/autossh/quartz/controller/QuartzController.java
@@ -1,8 +1,9 @@
 package com.autossh.quartz.controller;
 
-import com.autossh.config.quartz.QuartzManager;
 import com.autossh.quartz.service.QuartzService;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,9 @@ public class QuartzController {
     @Autowired
     private QuartzService quartzService;
 
+    @Autowired @Qualifier("Scheduler")
+    private Scheduler scheduler;
+
 
     /**
      * 更新单个定时任务
@@ -26,7 +30,7 @@ public class QuartzController {
      * @param
      */
     @GetMapping("/refresh")
-    public void refresh(String jobName, String jobGroup, String triggerName, String triggerGroup,String cron){
+    /*public void refresh(String jobName, String jobGroup, String triggerName, String triggerGroup,String cron){
         try {
            // QuartzManager quartzManager = new QuartzManager();
             QuartzManager.modifyJobTime(jobName,jobGroup,triggerName,triggerGroup,cron);
@@ -34,5 +38,32 @@ public class QuartzController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }*/
+    public void rescheduleJob(String jobName, String jobGroup, String cron) throws Exception
+    {
+        jobreschedule(jobName, jobGroup, cron);
     }
+
+    public void jobreschedule(String jobName, String jobGroup, String cron) throws Exception
+    {
+        try {
+            TriggerKey triggerKey = TriggerKey.triggerKey(jobName, jobGroup);
+            // 表达式调度构建器
+            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(cron);
+
+            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+
+            // 按新的cronExpression表达式重新构建trigger
+            trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
+
+            // 按新的trigger重新设置job执行
+            scheduler.rescheduleJob(triggerKey, trigger);
+        } catch (SchedulerException e) {
+            System.out.println("更新定时任务失败"+e);
+            throw new Exception("更新定时任务失败");
+        }
+    }
+
+
+
 }
